efda2d85cd1dd5683160c751755568b0d87d2c69
Pushker-stark <pushkernaresh0607@gmail.com>
2024-01-27 03:32:43 +0530

    docker file added

--- CredAPSystem/settings.py
+++ CredAPSystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'CredAPSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CreditCard',
-        'USER': 'pushker',
-        'PASSWORD': 'pushker',
-        'HOST': 'localhost',  # Set to the IP or hostname of your PostgreSQL server
-        'PORT': '5432',       # Default PostgreSQL port
+        'NAME': os.environ.get("SQL_NAME"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("HOST"),  # Set to the IP or hostname of your PostgreSQL server
+        'PORT': os.environ.get("PORT"),  # Default PostgreSQL port
     }
 }
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py runserver 127.0.0.1:8000
